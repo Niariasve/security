a251c913147682d85800f472e92ec2cee12aa402
Niariasve <138833714+Niariasve@users.noreply.github.com>
2024-07-22 14:39:44 -0500

    Guita 19 stop 2

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+let crypto = require('crypto');
+
 const sequelize = require('../models/index.js').sequelize;
 var initModels = require("../models/init-models");
 var models = initModels(sequelize);
@@ -12,5 +14,21 @@ router.get('/', async function(req, res, next) {
 
   res.render('crud', {title: 'CRUD with users', usersArray: usersCollection});
 });
+
+router.post('/', async function(req, res) {
+  
+
+  let { name, password, idrole } = req.body;
+
+  try {
+    let salt = process.env.SALT;
+    let hash = crypto.createHmac('sha512', salt).update(password).digest("base64");
+    let passwordHash = salt + "$" + hash;
+    let user = await models.users.create({ name: name, password: passwordHash});
+    res.redirect("http://www.google.com");
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
 
 module.exports = router;
