91102fb5b40241a858603dc35daebae94b1a112d
Niariasve <138833714+Niariasve@users.noreply.github.com>
2024-08-04 22:56:59 -0500

    Show token

--- routes/token.js
+++ routes/token.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
 router.get('/', async function(req, res, next) {
-    res.render('ticket', {username: req.cookies['username']})
+    const payload = {
+        data: 'secret info shhh'
+    }
+
+    const secret = process.env.TOKEN_SECRET
+    const token = jwt.sign(payload, secret, {
+        expiresIn: '1h'
+    })
+
+    res.render('ticket', {username: req.cookies['username'], token})
 })
 
 module.exports = router;
