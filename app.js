d16626aca591daa48c10fc58652ed8df1f91d39b
Niariasve <138833714+Niariasve@users.noreply.github.com>
2024-08-04 20:05:53 -0500

    user token view

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var authorizationSession = require('./middleware/authorization_session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tokenRouter = require('./routes/token');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', authenticateSession, authorizationSession, usersRouter);
+app.use('/token', authenticateSession, tokenRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
